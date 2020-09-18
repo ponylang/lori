4fe23e670123262b6559c300a7625a8e8ec8a3f3
Sean T. Allen <sean@seantallen.com>
2020-09-18 16:31:14 -0400

    Use latest ponyc release via ponyup for macOS CI testing

---
+++ .ci-scripts/macOS-install-release-pony-tools.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Libressl is required by ponyup
+#
+
+# get the latest version of libressl
+brew update
+brew install libressl
+
+#
+# Install ponyup
+#
+
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh | sh
+
+export PATH="$HOME/.local/share/ponyup/bin/:$PATH"
+
+ponyup update ponyc release
